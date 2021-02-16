1a380a6c5262c415f2175f8618c6898b76339cbb
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-02-16 15:13:43 -0400

    Create appbar and container

--- user/dashboard/src/views/Home.vue
+++ user/dashboard/src/views/Home.vue
@@ -1,13 +1,33 @@
 <template>
-  <div>
-    <p> Home view </p>
-  </div>
+  <Layout>
+    <h1>Hello, world!</h1>
+    <v-row no-gutters>
+      <v-col
+        v-for="n in 3"
+        :key="n"
+        cols="12"
+        sm="4"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          One of three columns
+        </v-card>
+      </v-col>
+    </v-row>
+  </Layout>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import Layout from "./Layout.vue";
 
 @Component({
+  components: { Layout }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+}
 </script>
+
