7b725e26346bbd26b222346612847a9a14243965
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-02-18 17:10:39 -0400

    [refactor] Move layout files to its corresponding directory

--- user/dashboard/src/views/Home.vue
+++ user/dashboard/src/views/Home.vue
@@ -1,33 +1,19 @@
 <template>
-  <Layout>
+  <div>
     <h1>Hello, world!</h1>
     <v-row no-gutters>
-      <v-col
-        v-for="n in 3"
-        :key="n"
-        cols="12"
-        sm="4"
-      >
-        <v-card
-          class="pa-2"
-          outlined
-          tile
-        >
+      <v-col v-for="n in 3" :key="n" cols="12" sm="4">
+        <v-card class="pa-2" outlined tile>
           One of three columns
         </v-card>
       </v-col>
     </v-row>
-  </Layout>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import Layout from "./Layout.vue";
+import { Component, Vue } from "vue-property-decorator";
 
-@Component({
-  components: { Layout }
-})
-export default class Home extends Vue {
-}
+@Component({ })
+export default class Home extends Vue {}
 </script>
-
