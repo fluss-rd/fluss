3e042ffb578982cd6491fc6279418a498d44490d
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-02-18 17:35:25 -0400

    [WIP] Changing text of Home page for testing pre-commit hook

--- user/dashboard/src/views/Home.vue
+++ user/dashboard/src/views/Home.vue
@@ -4,7 +4,7 @@
     <v-row no-gutters>
       <v-col v-for="n in 3" :key="n" cols="12" sm="4">
         <v-card class="pa-2" outlined tile>
-          One of three columns
+          Hey, I'm here
         </v-card>
       </v-col>
     </v-row>
