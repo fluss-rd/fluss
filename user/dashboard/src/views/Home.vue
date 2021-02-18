dc23c3ba0306d9d3f85bb997d87909a5badc47ad
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-02-18 17:40:12 -0400

    [WIP] :O

--- user/dashboard/src/views/Home.vue
+++ user/dashboard/src/views/Home.vue
@@ -4,7 +4,7 @@
     <v-row no-gutters>
       <v-col v-for="n in 3" :key="n" cols="12" sm="4">
         <v-card class="pa-2" outlined tile>
-          fdjfkaslfjsal
+          Hey :O
         </v-card>
       </v-col>
     </v-row>
