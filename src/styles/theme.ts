34c1d221833d94e1adbb35253a912255e2bb0f77
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-02-28 16:08:03 -0400

    [WIP] Install Nunito font

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,9 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
-const theme = createMuiTheme({});
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ["nunito"].join(","),
+  },
+});
 
 export default theme;
