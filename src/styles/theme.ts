e287fd44e64778bd02afe53c2965d71f03e9b15e
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-02-28 16:28:21 -0400

    Update navbar and theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,11 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
+  palette: {
+    primary: { main: "#1f6485" },
+    secondary: { main: "#7db6d1" },
+    background: { default: "#ffffff" },
+  },
   typography: {
     fontFamily: ["nunito"].join(","),
   },
