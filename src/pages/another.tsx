08f0a633980d7bc9d17487380ee11b3ff9c7f44a
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-03-03 19:20:57 -0400

    [WIP] Use another.tsx as UI example

--- src/pages/another.tsx
+++ src/pages/another.tsx
@@ -1,5 +1,25 @@
-import { Typography } from "@material-ui/core";
+import { Button, Card, CardActions, CardContent, Typography } from "@material-ui/core";
 
 export default function Another() {
-  return <Typography variant="h4">Another</Typography>;
+  return (
+    <Card>
+      <CardContent>
+        <Typography color="secondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+          be*nev*o*lent
+        </Typography>
+        <Typography color="secondary">adjective</Typography>
+        <Typography variant="body2" component="p">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore ipsam sequi qui,
+          necessitatibus quia voluptate. Quo et ad repudiandae animi natus. Soluta omnis natus
+          maiores voluptatem nam voluptas, libero sed!
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
 }
