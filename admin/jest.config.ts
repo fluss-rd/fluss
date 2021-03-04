4647ef1891cf1f5d16eed233688733bcfcb82eeb
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-03-03 20:54:17 -0400

    Add admin app code

---
+++ admin/jest.config.ts
@@ -0,0 +1,19 @@
+// jest.config.ts
+import type { Config } from "@jest/types";
+
+// Sync object
+const config: Config.InitialOptions = {
+  verbose: true,
+  testMatch: ["**/tests/**/*.(ts|tsx)"],
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/mocks/file-mock.js",
+    "\\.(css|scss)$": "<rootDir>/mocks/style-mock.js",
+  },
+};
+
+export default config;
---
+++ admin/src/layouts/main-layout/index.tsx
@@ -0,0 +1,38 @@
+import { Container, CssBaseline, Toolbar } from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import React, { FC } from "react";
+
+import theme from "../../styles/theme";
+import FlussAppBar from "./fluss-app-bar";
+
+const MainLayout: FC = ({ children }) => {
+  const classes = useStyles();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <FlussAppBar />
+      {children ? (
+        <>
+          <Toolbar />
+          <Container maxWidth="lg" className={classes.container}>
+            {children}
+          </Container>
+        </>
+      ) : (
+        children
+      )}
+    </ThemeProvider>
+  );
+};
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      paddingTop: theme.spacing(4),
+    },
+  })
+);
+
+export default MainLayout;
