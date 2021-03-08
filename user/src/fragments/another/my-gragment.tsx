512178f70b0e8ad4f3534b64951b3f578b914973
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-03-08 12:57:34 -0400

    [dbg] Add a fragment example and remove

---
+++ user/src/fragments/another/my-gragment.tsx
@@ -0,0 +1,3 @@
+export default function MyFragment() {
+  return <div>This is a fragment used for Another page.</div>;
+}
--- user/src/pages/another.tsx
+++ user/src/pages/another.tsx
@@ -1,5 +1,11 @@
+import MyFragment from "../fragments/another/my-gragment";
 import styles from "../styles/pages/another.module.scss";
 
 export default function Another() {
-  return <div className={styles.myStyle}>Hola</div>;
+  return (
+    <>
+      <div className={styles.myStyle}>Hola</div>
+      <MyFragment />
+    </>
+  );
 }
