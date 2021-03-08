fedc25421d5e411ec3b1e8cad35ca4d21c92a679
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-03-08 14:07:44 -0400

    [dbg] Delete unused sample pages and add extensions.json

--- admin/src/pages/index.tsx
+++ admin/src/pages/index.tsx
@@ -1,14 +1,5 @@
 import { Typography } from "@material-ui/core";
 
-import MyFragment from "../fragments/home/my-fragment";
-import styles from "../styles/pages/home.module.scss";
-
 export default function Home() {
-  return (
-    <div>
-      <Typography variant="h4">Hello, world : D!</Typography>;
-      <span className={styles.myStyle}>Hey, estoy estilizado</span>
-      <MyFragment />
-    </div>
-  );
+  return <Typography variant="h4">Hello, world : D!</Typography>;
 }
--- user/src/pages/index.tsx
+++ user/src/pages/index.tsx
@@ -1,18 +1,5 @@
-import { Button, Typography } from "@material-ui/core";
-import { FC } from "react";
+import { Typography } from "@material-ui/core";
 
-import { connect, StoreProps } from "../store";
-
-const Home: FC<StoreProps> = ({ store }) => {
-  return (
-    <>
-      <Typography variant="h4">Hello, world :D!{"<->"}</Typography>
-      <br />
-      <Button variant="outlined" color="secondary" onClick={() => store.counter++}>
-        {store.counter}
-      </Button>
-    </>
-  );
-};
-
-export default connect(Home);
+export default function Home() {
+  return <Typography variant="h4">Hello, world :D!{"<->"}</Typography>;
+}
