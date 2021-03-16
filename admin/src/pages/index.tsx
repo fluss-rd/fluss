0697d814a2c01cfb5e28f761f4f52fe206b5f3e9
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-03-15 23:46:59 -0400

    [fix] Use previuos text in home page

--- admin/src/pages/index.tsx
+++ admin/src/pages/index.tsx
@@ -2,15 +2,5 @@ import { Typography } from "@material-ui/core";
 import React from "react";
 
 export default function Home() {
-  console.log("hey");
-  const lines = [];
-  for (let i = 0; i < 50; i++) {
-    lines.push(
-      <Typography variant="h4" key={i}>
-        Hello, world : D!
-      </Typography>
-    );
-  }
-
-  return lines;
+  return <Typography variant="h4">Hello, world : D!</Typography>;
 }
