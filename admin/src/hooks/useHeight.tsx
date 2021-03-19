231ff3e7e360f2b0bbf91d40620aa36b999211a1
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-03-19 01:15:46 -0400

    [fix] Solve some issues with useHeight hook

--- admin/src/hooks/useHeight.tsx
+++ admin/src/hooks/useHeight.tsx
@@ -1,14 +1,45 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { MutableRefObject, useLayoutEffect, useRef, useState } from "react";
 
-export default function useHeight(): [MutableRefObject<any>, number] {
-  const [height, setHeight] = useState(0);
+type Size = "height" | "width";
+
+interface Options {
+  threshold?: number;
+  isMaxSize?: boolean;
+}
+
+export default function useSize(
+  sizeType: "height" | "width",
+  options: Options = { isMaxSize: true, threshold: 500 }
+): [MutableRefObject<any>, number] {
+  const [size, setSize] = useState(0);
   const ref = useRef(null);
 
-  useLayoutEffect(() => {
-    const newHeight = ref.current ? ref.current.clientHeight : 0;
-    if (ref.current && ref.current.clientHeight) setHeight(newHeight);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [ref.current, ref.current ? ref.current.clientHeight : 0]);
+  useLayoutEffect(updateValues, [ref.current, getSize(ref, sizeType)]);
 
-  return [ref, height];
+  function updateSize() {
+    const newSize = getSize(ref, sizeType);
+    if (options.isMaxSize && newSize > size) setSize(newSize);
+    else if (!options.isMaxSize && newSize !== size) setSize(newSize);
+  }
+
+  function updateSizeOnResize() {
+    setTimeout(updateSize, options.threshold);
+  }
+
+  function updateValues() {
+    updateSize();
+
+    if (window === undefined) return;
+
+    window.addEventListener("resize", updateSizeOnResize);
+    return () => window.removeEventListener("resize", updateSizeOnResize);
+  }
+
+  return [ref, size];
+}
+
+function getSize(ref: MutableRefObject<any>, size: Size) {
+  if (ref.current) return size === "width" ? ref.current.clientWidth : ref.current.clientHeight;
+  else return 0;
 }
