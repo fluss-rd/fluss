941d82ad949497b435a83274558eb138131ac8f0
Erica Ross <ericaross@Ericas-MacBook-Pro.local>
2021-04-01 18:37:14 -0400

    made changes to index page and added .sccs file

--- user/src/pages/another.tsx
+++ user/src/pages/another.tsx
@@ -4,7 +4,7 @@ import styles from "../styles/pages/another.module.scss";
 export default function Another() {
   return (
     <div>
-      <div className={styles.myStyle}>Hola</div>
+      <div className={styles["my-style"]}>Hola</div>
       <MyFragment />
     </div>
   );
--- user/src/pages/index.tsx
+++ user/src/pages/index.tsx
@@ -1,5 +1,40 @@
-import { Typography } from "@material-ui/core";
+import "fontsource-roboto";
+
+import { Button, Typography } from "@material-ui/core";
+
+import styles from "../styles/pages/index.module.scss";
 
 export default function Home() {
-  return <Typography variant="h4">Hello, world :D!{"<->"}</Typography>;
+  return (
+    <div className={styles["structure"]}>
+      <div className={styles["header-item"]}>
+        <Typography variant="caption" className={styles["content"]}>
+          Fluss
+        </Typography>
+        <Typography variant="h5" className={styles["bolds"]}>
+          Bienvenido a la pagina de datos de Rios en la Republica Dominicana
+        </Typography>
+        <br></br>
+        <Typography variant="body2">
+          En la plataforma podrás encontrar datos de parámetros{" "}
+        </Typography>
+        <Typography variant="body2">
+          fisicoquímicos del agua de los ríos de la República Dominicana
+        </Typography>
+        <br></br>
+        <Button color="primary" variant="outlined" className={styles["button"]}>
+          Datos y reportes
+        </Button>
+      </div>
+
+      <div className={styles["header-item"]}>
+        <img
+          src="https://i.ibb.co/yRtkvd4/Screen-Shot-2021-04-01-at-12-12-39-PM.png"
+          alt="River"
+          width="400"
+          height="300"
+        ></img>
+      </div>
+    </div>
+  );
 }
