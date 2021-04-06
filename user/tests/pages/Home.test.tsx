7e082a222358ac05f0862a4a0a9bd00a0c606d0d
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-04-05 22:49:30 -0400

    [fix] Add home page tests

--- user/tests/pages/Home.test.tsx
+++ user/tests/pages/Home.test.tsx
@@ -4,7 +4,9 @@ import { render } from "@testing-library/react";
 
 import Home from "../../src/pages";
 
-test("Check for Getting Started Text", () => {
+test("Check if welcome message is there", () => {
   const { getByText } = render(<Home />);
-  expect(getByText("Hello, world :D!<->")).toBeInTheDocument();
+  expect(
+    getByText("Bienvenido a la pagina de datos de Rios en la Republica Dominicana")
+  ).toBeInTheDocument();
 });
