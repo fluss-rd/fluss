91f528f1dd5890a911ba6d46cf79c16b62b7cb11
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-04-20 10:15:54 -0400

    [feat] Add more access to TabPanel elements

--- shared/components/TabPanel/index.tsx
+++ shared/components/TabPanel/index.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@material-ui/core";
+import { Box, BoxProps } from "@material-ui/core";
+import { CSSProperties } from "react";
 
 import AntTabs, { AntTab } from "./AntTabs";
 import HorizontalIconTab from "./HorizontalIconTab";
@@ -7,10 +8,15 @@ interface TabPanelProps {
   children?: React.ReactNode;
   index: any;
   value: any;
+  padding?: number;
+  style?: CSSProperties;
+  className?: string;
+  BoxProps?: BoxProps;
 }
 
 function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
+  const { children, value, index, padding, ...other } = props;
+  console.log(padding)
 
   return (
     <div
@@ -18,12 +24,23 @@ function TabPanel(props: TabPanelProps) {
       hidden={value !== index}
       id={`vertical-tabpanel-${index}`}
       aria-labelledby={`vertical-tab-${index}`}
+      style={{ height: "100%" }}
       {...other}
     >
-      {value === index && <Box p={3}>{children}</Box>}
+      {value === index && (
+        <Box p={padding} {...other.BoxProps}>
+          {children}
+        </Box>
+      )}
     </div>
   );
 }
+
+TabPanel.defaultProps = {
+  padding: 3,
+  BoxProps: {},
+  styles: {},
+};
 
 export default TabPanel;
 export { AntTab, AntTabs, HorizontalIconTab };
