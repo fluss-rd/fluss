44def6d1b2b55d005583dd0e8ab93733fe421ed8
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-04-19 21:05:11 -0400

    [refactor] Improve some FlussDrawerFoot styles

--- admin/src/layouts/MainLayout/FlussDrawerFoot.tsx
+++ admin/src/layouts/MainLayout/FlussDrawerFoot.tsx
@@ -1,18 +1,21 @@
 import {
+  Button,
   IconButton,
   List,
   ListItem,
   ListItemSecondaryAction,
   ListItemText,
 } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { PowerSettingsNew } from "@material-ui/icons";
 import { useGetUserData, useLogOut } from "hooks/auth-service";
+import clsx from "clsx";
 
 export default function FlussDrawerFoot() {
+  const classes = useStyles();
   const logOutMutation = useLogOut();
   const userQuery = useGetUserData();
   const response = userQuery.data;
-
   const userName =
     userQuery.isSuccess && response && response.data
       ? `${response.data.name} ${response.data.surname}`
@@ -23,9 +26,9 @@ export default function FlussDrawerFoot() {
   return (
     <List>
       <ListItem>
-        <ListItemText primary={userName} />
+        <Button className={classes.buttons}>{userName}</Button>
         <ListItemSecondaryAction>
-          <IconButton onClick={logOut}>
+          <IconButton onClick={logOut} className={classes.buttons}>
             <PowerSettingsNew />
           </IconButton>
         </ListItemSecondaryAction>
@@ -33,3 +36,12 @@ export default function FlussDrawerFoot() {
     </List>
   );
 }
+
+const useStyles = makeStyles((theme) => ({
+  buttons: {
+    textTransform: "capitalize",
+    margin: 0,
+    padding: 0,
+  },
+}));
+
