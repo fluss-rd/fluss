a8b92f51dbe4b156a0639333070ccca8dcbfdb9f
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-04-26 14:58:37 -0400

    [refactor] Split ViewModule UI and logic

--- admin/src/fragments/modulos/ViewModule.tsx
+++ admin/src/fragments/modulos/ViewModule.tsx
@@ -1,4 +1,3 @@
-import { yupResolver } from "@hookform/resolvers";
 import {
   AppBar,
   Button,
@@ -6,28 +5,20 @@ import {
   Divider,
   Grid,
   IconButton,
-  MenuItem,
   Toolbar,
   Typography,
 } from "@material-ui/core";
 import { makeStyles, Theme } from "@material-ui/core/styles";
-import { Close, EditLocationOutlined, InfoOutlined } from "@material-ui/icons";
+import { Close } from "@material-ui/icons";
 import { useGetModule } from "hooks/modules-service";
-import { useGetRivers } from "hooks/rivers-service";
 import Module from "models/Module";
-import River from "models/River";
-import { ChangeEvent, FC, useEffect } from "react";
-import { Controller, useForm } from "react-hook-form";
-import ReactInputMask from "react-input-mask";
-import { ModuleForm } from "services/modules/models";
+import { FC, useEffect } from "react";
 import FormField from "shared/components/FormField";
-import FormIconTitle from "shared/components/FormIconTitle";
-import FormSelect from "shared/components/FormSelect";
 import Transition from "shared/components/Transition";
 import { formatDate } from "shared/helpers";
-import * as yup from "yup";
 
-import ModuleLocation from "./ModuleLocation";
+import ModuleForm from "./ModuleForm";
+import useModuleForm from "./useModuleForm";
 
 interface ViewModuleProps {
   open?: boolean;
@@ -37,56 +28,31 @@ interface ViewModuleProps {
 
 const ViewModule: FC<ViewModuleProps> = (props) => {
   const classes = useStyles();
-  const form = useForm<ModuleForm>({ resolver: yupResolver(formSchema) });
-  const { handleSubmit, errors, register, reset, control, setValue } = form;
-  const riversQuery = useGetRivers({ enabled: false });
+  const formLogic = useModuleForm();
   const moduleQuery = useGetModule(props.moduleId, {
     enabled: false,
     onSuccess: (response) => {
       // Initialize module form initial data.
       if (response?.data) {
         const defaultValues = Module.toModuleForm(response.data);
-        reset(defaultValues);
+        formLogic.form.reset(defaultValues);
       }
     },
   });
   const module: Module = moduleQuery.data?.data || ({} as Module);
-  const rivers: River[] = riversQuery.data?.data || [];
+  const onSubmit = (data: any) => console.log(data);
 
   // Refetch module data on dialog opening.
   useEffect(() => {
     if (props.open) {
       moduleQuery.refetch();
-      riversQuery.refetch();
+      formLogic.riversQuery.refetch();
     }
   }, [props.open]);
 
-  // Register riverId to the form.
-  useEffect(() => {
-    if (props.open) register("riverId");
-  }, [register]);
-
-  // Update the module data when the form is submitted.
-  const onSubmit = () => (data: ModuleForm) => console.log(data);
-
-  // Update the riverId value when the river name changes.
-  const onRiverIdChange = (e: ChangeEvent<{ name: string; value: string }>) => {
-    if (!riversQuery.data?.data) return;
-
-    const riverName = e.target.value;
-    const riverMatch = riversQuery.data.data.find((river) => river.name === riverName);
-
-    setValue("riverId", riverMatch?.id || "", { shouldValidate: true });
-  };
-
-  const changeLocation = (latitude: number, longitude: number) => {
-    form.setValue("location.latitude", latitude);
-    form.setValue("location.longitude", longitude);
-  };
-
   return (
     <Dialog fullScreen open={props.open} onClose={props.close} TransitionComponent={Transition}>
-      <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+      <form noValidate autoComplete="off" onSubmit={formLogic.form.handleSubmit(onSubmit)}>
         <AppBar color="inherit" elevation={0} style={{ position: "relative" }}>
           <Toolbar>
             <IconButton edge="start" color="inherit" onClick={props.close} aria-label="close">
@@ -122,96 +88,7 @@ const ViewModule: FC<ViewModuleProps> = (props) => {
                 label="Última actualización"
               />
             </Grid>
-            {/* Data */}
-            <Grid item xs={12}>
-              <FormIconTitle Icon={InfoOutlined} title="Detalle" />
-              <Grid container spacing={2}>
-                <Grid item xs={12} md={6}>
-                  <Controller
-                    name="phoneNumber"
-                    defaultValue=""
-                    control={control}
-                    as={
-                      <ReactInputMask mask="(999) 999-9999">
-                        {() => (
-                          <FormField
-                            label="Numéro celular"
-                            error={!!errors.phoneNumber}
-                            helperText={errors.phoneNumber?.message}
-                          />
-                        )}
-                      </ReactInputMask>
-                    }
-                  />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                  <FormField
-                    name="serial"
-                    label="Serial"
-                    inputRef={register}
-                    error={!!errors.serial}
-                    helperText={errors.serial?.message}
-                  />
-                </Grid>
-              </Grid>
-            </Grid>
-            {/* Location */}
-            <Grid item xs={12}>
-              <FormIconTitle Icon={EditLocationOutlined} title="Ubicación" />
-              <Grid container spacing={2}>
-                <Grid item xs={12} md={6}>
-                  <Grid container spacing={2}>
-                    {moduleQuery.data?.data?.riverName && rivers.length && (
-                      <Grid item xs={12}>
-                        <FormSelect
-                          noneText="Ninguno"
-                          label="Cuerpo hídrico"
-                          error={!!errors.riverId}
-                          helperText={errors.riverId?.message}
-                          onChange={onRiverIdChange}
-                          defaultValue={module.riverName}
-                        >
-                          {rivers.map((river: River) => (
-                            <MenuItem key={river.id} value={river.name}>
-                              {river.name}
-                            </MenuItem>
-                          ))}
-                        </FormSelect>
-                      </Grid>
-                    )}
-                    <Grid item xs={12} md={6}>
-                      <FormField
-                        name="location.latitude"
-                        label="Latitud"
-                        type="number"
-                        inputRef={register}
-                        error={!!errors.location?.latitude}
-                        helperText={errors.location?.latitude?.message}
-                      />
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                      <FormField
-                        name="location.longitude"
-                        label="Longitud"
-                        type="number"
-                        inputRef={register}
-                        error={!!errors.location?.longitude}
-                        helperText={errors.location?.longitude?.message}
-                      />
-                    </Grid>
-                  </Grid>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                  <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                    <ModuleLocation onNewMarker={changeLocation} />
-                    <br />
-                    <Typography style={{ fontWeight: "bold" }} color="textSecondary">
-                      Ubicación
-                    </Typography>
-                  </div>
-                </Grid>
-              </Grid>
-            </Grid>
+            <ModuleForm moduleForm={formLogic} module={module} />
           </Grid>
         </div>
       </form>
@@ -242,23 +119,5 @@ const useStyles = makeStyles((theme: Theme) => ({
     flex: 1,
   },
 }));
-
-const formSchema: yup.SchemaOf<ModuleForm> = yup.object().shape({
-  phoneNumber: yup.string().required("Por favor, introduzca el número de teléfono del módulo"),
-  riverId: yup
-    .string()
-    .required("Por favor, seleccione el cuerpo hídrico donde el módulo se encuentra ubicado"),
-  serial: yup.string().required("Por favor, introduzca el serial del módulo"),
-  location: yup.object().shape({
-    latitude: yup
-      .number()
-      .typeError("La latitud ingresada noes válida, debe ser un número")
-      .required("Debe inidicar la latitud de la ubicación del módulo"),
-    longitude: yup
-      .number()
-      .typeError("La longitud ingresada noes válida, debe ser un número")
-      .required("Debe inidicar la longitud de la ubicación del módulo"),
-  }),
-});
 
 export default ViewModule;
