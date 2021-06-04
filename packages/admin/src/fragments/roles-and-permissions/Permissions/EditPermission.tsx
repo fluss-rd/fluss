3a79d13c6e6bb5882e6378a92135c44f6c74a778
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-06-04 11:28:38 -0400

    [refactor] Enable only view of permissions

--- packages/admin/src/fragments/roles-and-permissions/Permissions/EditPermission.tsx
+++ packages/admin/src/fragments/roles-and-permissions/Permissions/EditPermission.tsx
@@ -1,28 +1,29 @@
-import { Dialog, DialogTitle, Divider, IconButton } from "@material-ui/core";
+import { Dialog, DialogTitle, IconButton, Button, DialogActions } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { Info } from "@material-ui/icons";
 import Permission from "models/Permission";
 import React, { FC, useState } from "react";
+import FormField from "shared/components/FormField";
+import ModalContent from "shared/components/ModalContent";
 
 import ModalFormDates from "../ModalFormDates";
-import PermissionModalForm, { PermissionForm } from "./PermissionModalForm";
 
 interface EditPermissionProps {
   permission: Permission;
 }
 
 const EditPermission: FC<EditPermissionProps> = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
-
   const classes = useStyles();
-  const {
-    permission: { name, description, updatedAt, creationDate },
-  } = props;
+  const [isOpen, setIsOpen] = useState(false);
+  const { name, description, updatedAt, creationDate } = props.permission;
 
-  const openDialog = () => setIsOpen(true);
-  const closeDialog = () => setIsOpen(false);
-  const handleSave = (data: PermissionForm) => console.log(data);
-  const updateValues = (data: PermissionForm) => console.log(data);
+  const openDialog = () => {
+    setIsOpen(true);
+  };
+
+  const closeDialog = () => {
+    setIsOpen(false);
+  };
 
   return (
     <>
@@ -42,18 +43,26 @@ const EditPermission: FC<EditPermissionProps> = (props) => {
         <ModalFormDates lastUpdate={updatedAt} creationDate={creationDate} />
 
         <div className={classes.formSection}>
-          <PermissionModalForm
-            cancelForm={closeDialog}
-            onSaveForm={handleSave}
-            values={{ name, description }}
-          />
+          <ModalContent className={classes.content}>
+            <FormField label="Nombre" value={name} placeholder="Nombre del rol" />
+            <FormField
+              multiline
+              rows={5}
+              value={description}
+              label="Descripción"
+              placeholder="Descripción del permiso"
+            />
+          </ModalContent>
+          <DialogActions>
+            <Button onClick={closeDialog} color="primary">
+              Cerrar
+            </Button>
+          </DialogActions>
         </div>
       </Dialog>
     </>
   );
 };
-
-// Nombre, correo,
 
 EditPermission.defaultProps = {
   permission: {
@@ -70,6 +79,12 @@ const useStyles = makeStyles((theme) => ({
   formSection: {
     marginTop: theme.spacing(5),
   },
+  content: {
+    "& > *:not(:last-child)": {
+      marginBottom: theme.spacing(2),
+    },
+  },
 }));
 
 export default EditPermission;
+
