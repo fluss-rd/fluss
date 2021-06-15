098d197c051ed9325fad8c6c6f7a01d28033840f
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-06-15 14:18:43 -0400

    [feat] Add download data modal and add dynamic select to ModuleData buttons

--- packages/admin/src/fragments/watersheds/id/Modules/ModuleData.tsx
+++ packages/admin/src/fragments/watersheds/id/Modules/ModuleData.tsx
@@ -8,13 +8,15 @@ import {
   ButtonGroup,
 } from "@material-ui/core";
 import { Close, GetApp } from "@material-ui/icons";
-import { makeStyles } from "@material-ui/core/styles";
-import React, { FC, useMemo } from "react";
+import { makeStyles, useTheme, lighten, darken } from "@material-ui/core/styles";
+import React, { FC, useMemo, useState } from "react";
 import { mockDayMeasures } from "models/day-measures";
 import ModalContent from "shared/components/ModalContent";
 import { ResponsiveCalendar } from "@nivo/calendar";
 import { appBarHeight } from "shared/helpers";
 import { mockModules } from "models/module";
+import ModuleDataDownload from "./ModuleDataDownload";
+import useBoolean from "hooks/useBoolean";
 
 interface ModuleDataProps {
   moduleId: string;
@@ -23,77 +25,121 @@ interface ModuleDataProps {
 }
 
 const ModuleData: FC<ModuleDataProps> = ({ isOpen, close, moduleId }) => {
+  const [downloadDataIsOpen, openDownloadData, closeDownloadData] = useBoolean();
+  const [current, setCurrent] = useState(0);
   const classes = useStyles();
   const measures = useMemo(() => mockDayMeasures(), []);
   const module = mockModules().find((m) => m.id === moduleId);
 
+  const onParameterChange = (index: number) => {
+    setCurrent(index);
+  };
+
   return (
-    <Dialog
-      fullScreen
-      onClose={close}
-      open={isOpen}
-      maxWidth="lg"
-      aria-labelledby="form-dialog-title"
+    <>
+      <Dialog
+        fullScreen
+        onClose={close}
+        open={isOpen}
+        maxWidth="lg"
+        aria-labelledby="form-dialog-title"
+      >
+        <AppBar position="fixed" color="transparent" elevation={0} variant="outlined">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+              onClick={close}
+            >
+              <Close />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Datos de módulo
+            </Typography>
+            <Button color="primary" startIcon={<GetApp />} onClick={openDownloadData}>
+              Descargar datos
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <ModalContent className={classes.content}>
+          <div>
+            <ButtonGroup
+              size="large"
+              color="primary"
+              aria-label="large outlined primary button group"
+            >
+              <SelectButton index={0} value={current} onClick={onParameterChange}>
+                pH
+              </SelectButton>
+              <SelectButton index={1} value={current} onClick={onParameterChange}>
+                Oxígeno
+              </SelectButton>
+              <SelectButton index={2} value={current} onClick={onParameterChange}>
+                Temperatura
+              </SelectButton>
+              <SelectButton index={3} value={current} onClick={onParameterChange}>
+                Sólidos disueltos
+              </SelectButton>
+              <SelectButton index={4} value={current} onClick={onParameterChange}>
+                Turbidez
+              </SelectButton>
+            </ButtonGroup>
+          </div>
+          <ResponsiveCalendar
+            data={measures}
+            from="2015-03-01"
+            to="2016-07-12"
+            emptyColor="#eeeeee"
+            colors={["#61cdbb", "#97e3d5", "#e8c1a0", "#f47560"]}
+            yearSpacing={40}
+            monthBorderColor="#ffffff"
+            dayBorderWidth={2}
+            dayBorderColor="#ffffff"
+            margin={{ left: 40, right: 40 }}
+            legends={[
+              {
+                anchor: "bottom-right",
+                direction: "row",
+                translateY: 36,
+                itemCount: 4,
+                itemWidth: 42,
+                itemHeight: 36,
+                itemsSpacing: 14,
+                itemDirection: "right-to-left",
+              },
+            ]}
+          />
+        </ModalContent>
+      </Dialog>
+      <ModuleDataDownload isOpen={downloadDataIsOpen} close={closeDownloadData} />
+    </>
+  );
+};
+
+interface SelectButtonProps {
+  index: number;
+  value: number;
+  onClick: (index: number) => void;
+}
+
+const SelectButton: FC<SelectButtonProps> = ({ index, value, onClick, ...props }) => {
+  const theme = useTheme();
+  const selected = index === value;
+
+  const handleClick = () => {
+    onClick(index);
+  };
+
+  return (
+    <Button
+      style={{ background: selected && lighten(theme.palette.primary.main, 0.8) }}
+      onClick={handleClick}
+      {...props}
     >
-      <AppBar position="fixed" color="transparent" elevation={0} variant="outlined">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-            onClick={close}
-          >
-            <Close />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Datos de módulo
-          </Typography>
-          <Button color="primary" startIcon={<GetApp />}>
-            Descargar datos
-          </Button>
-        </Toolbar>
-      </AppBar>
-      <ModalContent className={classes.content}>
-        <div>
-          <ButtonGroup
-            size="large"
-            color="primary"
-            aria-label="large outlined primary button group"
-          >
-            <Button>pH</Button>
-            <Button>Oxígeno</Button>
-            <Button>Sólidos disueltos</Button>
-            <Button>Temperatura</Button>
-            <Button>Turbidez</Button>
-          </ButtonGroup>
-        </div>
-        <ResponsiveCalendar
-          data={measures}
-          from="2015-03-01"
-          to="2016-07-12"
-          emptyColor="#eeeeee"
-          colors={["#61cdbb", "#97e3d5", "#e8c1a0", "#f47560"]}
-          yearSpacing={40}
-          monthBorderColor="#ffffff"
-          dayBorderWidth={2}
-          dayBorderColor="#ffffff"
-          margin={{ left: 40, right: 40 }}
-          legends={[
-            {
-              anchor: "bottom-right",
-              direction: "row",
-              translateY: 36,
-              itemCount: 4,
-              itemWidth: 42,
-              itemHeight: 36,
-              itemsSpacing: 14,
-              itemDirection: "right-to-left",
-            },
-          ]}
-        />
-      </ModalContent>
-    </Dialog>
+      {props.children}
+    </Button>
   );
 };
 
---
+++ packages/admin/src/fragments/watersheds/id/Modules/ModuleDataDownload.tsx
@@ -0,0 +1,82 @@
+import {
+  FormHelperText,
+  FormControl,
+  Checkbox,
+  FormGroup,
+  FormLabel,
+  FormControlLabel,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  Typography,
+  DialogActions,
+  Radio,
+  Button,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import React, { FC } from "react";
+
+interface ModuleDataDownloadProps {
+  isOpen: boolean;
+  close: () => void;
+}
+
+const ModuleDataDownload: FC<ModuleDataDownloadProps> = ({ isOpen, close }) => {
+  const classes = useStyles();
+
+  const onCancel = () => {
+    close();
+  };
+
+  const onSave = () => {
+    console.log("onSave");
+  };
+
+  return (
+    <div>
+      <Dialog
+        disableBackdropClick
+        disableEscapeKeyDown
+        maxWidth="md"
+        onClose={close}
+        open={isOpen}
+        aria-labelledby="confirmation-dialog-title"
+      >
+        <DialogTitle id="confirmation-dialog-title">Descargar datos</DialogTitle>
+        <DialogContent dividers>
+          <FormControl component="fieldset">
+            <FormLabel component="legend">Elegir parámetros</FormLabel>
+            <FormGroup>
+              <FormControlLabel control={<Checkbox name="oxygen" />} label="Oxígeno" />
+              <FormControlLabel control={<Checkbox name="pH" />} label="pH" />
+              <FormControlLabel control={<Checkbox name="temperature" />} label="Temperatura" />
+              <FormControlLabel control={<Checkbox name="solids" />} label="Sólidos disueltos" />
+              <FormControlLabel control={<Checkbox name="turbidity" />} label="Turbidez" />
+            </FormGroup>
+            {/*<FormHelperText>Debes elegir al menos un parámetro para la descarga</FormHelperText>*/}
+          </FormControl>
+          <FormControl component="fieldset">
+            <FormLabel component="legend">Elegir formato</FormLabel>
+            <FormGroup>
+              <FormControlLabel control={<Radio name="excel" />} label="Excel" />
+              <FormControlLabel control={<Radio name="csv" />} label="CSV" />
+            </FormGroup>
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={onCancel} color="primary">
+            Cancelar
+          </Button>
+          <Button onClick={onSave} color="primary">
+            Descargar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+const useStyles = makeStyles({});
+
+export default ModuleDataDownload;
+
