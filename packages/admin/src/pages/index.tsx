5959d4fd96d4854bd17ba0b71a9613facdc08bf0
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-06-15 19:19:40 -0400

    [feat] Add ModuleStatuses

--- packages/admin/src/pages/index.tsx
+++ packages/admin/src/pages/index.tsx
@@ -1,18 +1,21 @@
 import { Grid } from "@material-ui/core";
 import UserCard from "fragments/home/UserCard";
 import ResumeByWatershed from "fragments/home/ResumeByWatershed";
+import ModuleStatuses from "fragments/home/ModuleStatuses";
 
 export default function Home() {
   return (
     <div>
-      <Grid container>
+      <Grid container spacing={3}>
         <Grid item xs={6}>
           <UserCard />
           <br />
           <ResumeByWatershed />
         </Grid>
 
-        <Grid item xs={6}></Grid>
+        <Grid item xs={6}>
+          <ModuleStatuses />
+        </Grid>
       </Grid>
     </div>
   );
