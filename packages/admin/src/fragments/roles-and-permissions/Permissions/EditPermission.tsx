3da3cae8bef864cfaab9f6445af9fb07731e7b0d
Mikhael1729 <Mikhael1729@gmail.com>
2021-07-11 23:18:37 -0400

    [refactor] Use new DataTable on Roles and Persmissions

--- packages/admin/src/fragments/roles-and-permissions/Permissions/EditPermission.tsx
+++ packages/admin/src/fragments/roles-and-permissions/Permissions/EditPermission.tsx
@@ -1,5 +1,4 @@
 import { Button, Dialog, DialogActions, DialogTitle, IconButton } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 import { Info } from "@material-ui/icons";
 import RegistrationAndUpdateDates from "components/RegistrarionAndUpdateDates";
 import Permission from "models/Permission";
--- packages/admin/src/fragments/roles-and-permissions/Roles/index.tsx
+++ packages/admin/src/fragments/roles-and-permissions/Roles/index.tsx
@@ -1,5 +1,5 @@
 import Role, { mockRoles } from "models/Role";
-import { DataTableColumn, EnhancedDataTable } from "shared/components/Tables";
+import DataTable, { DataTableColumn } from "shared/components/DataTable";
 import formatDate from "shared/helpers/formatDate";
 
 import CreateRol from "./CreateRole";
@@ -8,7 +8,7 @@ import EditRol from "./EditRole";
 export default function Roles() {
   return (
     <>
-      <EnhancedDataTable data={mockRoles()} columns={columns} />
+      <DataTable showGlobalFilter data={mockRoles()} columns={columns} />
       <CreateRol />
     </>
   );
@@ -21,3 +21,4 @@ const columns: DataTableColumn<Role>[] = [
   { Header: "Última actualización", accessor: (rol) => formatDate(rol.lastUpdate) },
   { Header: " ", columnWidth: "100px", id: "info", accessor: (rol) => <EditRol role={rol} /> },
 ];
+
