79922028a5b39fe89afca5a10fe85c859aadaa76
Mikhael1729 <Mikhael1729@gmail.com>
2021-07-12 22:22:16 -0400

    [fix] Show toolbar space only on home

--- packages/user/src/pages/monitor/index.tsx
+++ packages/user/src/pages/monitor/index.tsx
@@ -5,7 +5,6 @@ const Monitor = () => {
   const router = useRouter();
 
   const toModuleData = (moduleId: string) => {
-    console.log({ moduleId });
     router.push(`/monitor/${moduleId}`);
   };
 
