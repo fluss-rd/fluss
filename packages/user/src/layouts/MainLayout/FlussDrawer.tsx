da9d588f8eea0244d543015b4cc431f0d6c03077
Mikhael1729 <Mikhael1729@gmail.com>
2021-07-19 09:10:02 -0400

    [refactor] Make the appbar responsive

---
+++ packages/user/src/layouts/MainLayout/FlussDrawer.tsx
@@ -0,0 +1,44 @@
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import React, { FC } from "react";
+
+interface FlussDrawerProps {}
+
+interface FlussDrawerProps {
+  isOpen?: boolean;
+  close?: () => void;
+}
+
+const FlussDrawer: FC<FlussDrawerProps> = (props) => {
+  return (
+    <Drawer anchor="left" open={props.isOpen} onClose={props.close}>
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  );
+};
+
+export default FlussDrawer;
+
--- packages/user/src/layouts/MainLayout/index.tsx
+++ packages/user/src/layouts/MainLayout/index.tsx
@@ -33,3 +33,4 @@ const useStyles = makeStyles((theme: Theme) => ({
 }));
 
 export default MainLayout;
+
