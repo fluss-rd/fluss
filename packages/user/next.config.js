af8086720e62fa36a9b704ad1b7a629166b05d6b
Mikhael1729 <Mikhael1729@gmail.com>
2021-07-19 17:56:15 -0400

    [fix] Use https

--- packages/user/next.config.js
+++ packages/user/next.config.js
@@ -7,7 +7,7 @@ module.exports = withPlugins([withTM], {
   env: {
     mapboxToken:
       "pk.eyJ1IjoibWlraGFlbDE3MjkiLCJhIjoiY2ttbGN2Y2M1MTl3YjJ1bjAyZmg0MmU1NCJ9.WiU0fisWQSYwcEs-Ay6ONw",
-    apiUrl: "http://api.fluss.com.do",
+    apiUrl: "https://api.fluss.com.do",
   },
   webpack(config, options) {
     const { dev, isServer } = options;
