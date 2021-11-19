871962d8862f1ca0eeb5ec202c50f66343e9f272
Mikhael1729 <Mikhael1729@gmail.com>
2021-11-18 20:40:20 -0400

    [feat] Add types for regenerator-runtime

--- packages/admin/src/pages/_app.tsx
+++ packages/admin/src/pages/_app.tsx
@@ -3,11 +3,9 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import "regenerator-runtime/runtime";
 
 import DateFnsUtils from "@date-io/date-fns";
-import { createMuiTheme } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import Layout from "layouts/Layout";
-import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
@@ -45,19 +43,6 @@ function MyApp({ Component, pageProps }) {
     </QueryClientProvider>
   );
 }
-
-//const monitorTheme = createMuiTheme({
-  //...theme,
-  //palette: {
-    //type: "dark",
-    //primary: {
-      //main: "#FFFFFF",
-    //},
-    //secondary: {
-      //main: "#7db6d1",
-    //},
-  //},
-//});
 
 export default MyApp;
 
